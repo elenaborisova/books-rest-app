c9d597a264a9cf8a9487f43815d986a24a09318e
Elena Borisova <elenaborrisova@gmail.com>
2022-09-30 00:21:37 +0200

    add new methods in AuthorController

--- src/main/java/com/example/booksrestapp/web/AuthorController.java
+++ src/main/java/com/example/booksrestapp/web/AuthorController.java
@@ -1,21 +1,22 @@
 package com.example.booksrestapp.web;
 
 import com.example.booksrestapp.models.entities.AuthorEntity;
-import com.example.booksrestapp.repositories.AuthorRepository;
 import com.example.booksrestapp.repositories.AuthorSearchSpecification;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.booksrestapp.services.AuthorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AuthorController {
 
-    private final AuthorRepository authorRepository;
+    private final AuthorService authorService;
 
-    public AuthorController(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
     }
 
     @GetMapping("/search")
@@ -26,11 +27,38 @@ public class AuthorController {
                 bookTitle
         );
 
-        return authorRepository.findAll(searchSpecification);
+        return authorService.getAllAuthors(searchSpecification);
     }
 
     @GetMapping
     public List<AuthorEntity> getAuthors() {
-        return authorRepository.findAll();
+        return authorService.getAllAuthors();
+    }
+
+    @GetMapping("/{authorId}")
+    public ResponseEntity<AuthorEntity> getAuthor(@PathVariable long authorId) {
+
+        Optional<AuthorEntity> author = authorService.getAuthorById(authorId);
+
+        return author
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<AuthorEntity> create(UriComponentsBuilder ucBuilder,
+                                               @RequestBody AuthorEntity author) {
+
+        AuthorEntity newAuthor = authorService.saveNewAuthor(author);
+
+        return ResponseEntity
+                .created(ucBuilder.path("/authors/{authorId}")
+                        .buildAndExpand(newAuthor.getId()).toUri()).build();
+    }
+
+    @DeleteMapping("/{authorId}")
+    public ResponseEntity<AuthorEntity> delete(@PathVariable Long authorId) {
+        authorService.deleteAuthorById(authorId);
+        return ResponseEntity.noContent().build();
     }
 }
